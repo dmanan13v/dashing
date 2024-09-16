975b64999362384a977fd7e59c4333649e6f4709
dmananiev <93711930+dmananiev@users.noreply.github.com>
2024-09-16 14:05:59 +0100

    add gunicorn

--- the_application.py
+++ the_application.py
@@ -5,11 +5,10 @@ from layouts import landing_layout, main_app_layout
 
 # Initialize the Dash app
 app = dash.Dash(__name__, assets_folder="assets", suppress_callback_exceptions=True)
-
+server = app.server
 app.layout = html.Div(
     [dcc.Location(id="url", refresh=False), html.Div(id="page-content")]
 )
-
 
 @app.callback(Output("page-content", "children"), Input("url", "pathname"))
 def display_page(pathname):
@@ -21,7 +20,3 @@ def display_page(pathname):
 
 # Register callbacks
 register_callbacks(app)
-
-if __name__ == "__main__":
-    app.run_server(debug=False)
-    # app.run_server(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
